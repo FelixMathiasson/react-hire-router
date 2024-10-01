585781c5d41ff7cb1a0832b36f31e178d94a9ab8
Felix <felix.mathiasson99@gmail.com>
2024-10-01 15:14:54 +0200

    extension done

--- src/pages/Dashboard/components/PeopleList.jsx
+++ src/pages/Dashboard/components/PeopleList.jsx
@@ -1,9 +1,11 @@
 import PeopleListItem from './PeopleListItem'
 
+// eslint-disable-next-line react/prop-types
 function PeopleList({people, hired}) {
 
   return (
     <ul>
+      {/* eslint-disable-next-line react/prop-types*/}
       {people.map((person) => (
         <PeopleListItem key={person.email} person={person} hired={hired} />
       ))}
